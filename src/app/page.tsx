d6ef0cdcdf4367f4cc46c2a910bf641adb5d4b40
heudev <hulkienesuysal@gmail.com>
2025-06-02 21:19:52 +0300

    development area

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,14 +23,13 @@ export default function Home() {
       ></div>
       <div className="fixed inset-0 bg-black opacity-65 z-10"></div>
       <section className="relative z-20">
-        <Navbar />
+        {/* <Navbar />
         <Hero />
         <div className="space-y-20 md:space-y-32">
           <Letter />
           <Committees />
           <ConferenceDays />
           <Fees />
-          {/* <Team /> */}
           <Gallery />
           <Youtube />
           <Faq />
@@ -38,7 +37,19 @@ export default function Home() {
         </div>
         <div className="flex justify-center pb-5 pt-5">
           <a href="https://enes.run" target="_blank" className="italic">Developed by <span className="font-bold">Enes Uysal</span></a>
+        </div> */}
+
+        <div className="h-screen text-white flex flex-col justify-center items-center px-4 text-center">
+          <h1 className="text-4xl md:text-6xl font-bold mb-6 animate-pulse">
+            Coming Back Stronger Than Ever
+          </h1>
+          <p className="text-lg md:text-2xl mb-10 opacity-80">
+            We’re rebuilding something great. Stay tuned and stay with us.
+          </p>
+          <hr className="w-32 border-t-2 border-white mb-6" />
+          <div className="text-xl md:text-2xl font-medium tracking-wide">ECOMUN Team</div>
         </div>
+
       </section>
     </main>
   );
